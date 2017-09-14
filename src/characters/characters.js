3cd7148bb9da886284887c921569fc15a138942a
Jeremy Gonzalez <jeremygonzalez@outlook.com>
2017-09-14 16:09:14 -0700

    add characters and stats

---
+++ src/characters/characters.js
@@ -0,0 +1,9 @@
+import { Aya } from './aya'
+import { Barks } from './barks'
+import { Poff } from './poff'
+import { Ress } from './ress'
+import { Zayin } from './Zayin'
+
+export const Characters = [
+  Ress, Barks, Aya, Poff, Zayin
+]
